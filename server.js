cafb8b4f35fa0c37c78e1ff307a823564d5cae34
vitaliyItDevops <vitaliyworkit@gmail.com>
2024-10-30 00:26:09 +0200

    selected domain

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 
 
 const allowedOrigins = [
-    'https://52fc-188-163-45-97.ngrok-free.app',
+    'https://1faf-188-163-45-97.ngrok-free.app',
     'https://web.telegram.org' // если нужно разрешить и Telegram WebApp
 ];
 
